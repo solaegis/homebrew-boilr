15bd3bb57a7aa7bb977cc80ed3eff6ef5a021050
iahidar <iahidar@signet.nl>
2018-08-16 14:14:23 +0200

    Updated url

--- Formula/boilr.rb
+++ Formula/boilr.rb
@@ -1,7 +1,7 @@
 class Boilr < Formula
     desc "Boilerplate template manager that generates files or directories from template repositories"
     homepage "https://github.com/Ilyes512/boilr"
-    url "https://github.com/Ilyes512/boilr/releases/download/0.4.3/boilr-0.4.3-darwin_amd64.tgz"
+    url "https://github.com/Ilyes512/boilr/releases/download/0.4.4/boilr-0.4.4-darwin_amd64.tgz"
     version "0.4.4"
     sha256 "0b943271f689bf35626df6c9c1ad83fb4bdac85dec374964883ce1ec818da8c3"
     head "https://github.com/Ilyes512/boilr.git"
