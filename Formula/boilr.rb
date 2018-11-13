64389b5d74645f991d917a56f6a38d812b98d453
goreleaserbot <ilyes.ahidar@gmail.com>
2018-11-13 23:48:03 +0100

    Brew formula update for boilr version 0.4.5

--- Formula/boilr.rb
+++ Formula/boilr.rb
@@ -1,18 +1,11 @@
 class Boilr < Formula
-    desc "Boilerplate template manager that generates files or directories from template repositories"
-    homepage "https://github.com/Ilyes512/boilr"
-    url "https://github.com/Ilyes512/boilr/releases/download/0.4.4/boilr-0.4.4-darwin_amd64.tgz"
-    version "0.4.4"
-    sha256 "586dbf7cd20b33cf716dce5d5b74821f0fff1bd90d4a2a8ecd22dd43403cbbe2"
-    head "https://github.com/Ilyes512/boilr.git"
+  desc "Boilerplate template manager that generates files or directories from template repositories"
+  homepage "https://github.com/Ilyes512/boilr"
+  url "https://github.com/Ilyes512/boilr/releases/download/0.4.5/boilr_0.4.5_macOS_64-bit.tar.gz"
+  version "0.4.5"
+  sha256 "25c4e890e227602ba17abdd22cffdf18808c3bb5b81a3858d23eb1084103b495"
 
-    bottle :unneeded
-
-    def install
-        bin.install "boilr"
-    end
-
-    test do
-        assert_match version.to_s, shell_output("#{bin}/boilr version --dont-prettify")
-    end
+  def install
+    bin.install "boilr"
+  end
 end
