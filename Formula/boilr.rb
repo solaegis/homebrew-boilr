977bea48dfc0a1caa6a71e553555b7b225861596
Lewis Burgess <Lewis.Burgess@acxiom.com>
2022-05-12 11:56:08 -0400

    return-to-old-style

--- Formula/boilr.rb
+++ Formula/boilr.rb
@@ -1,13 +1,11 @@
-cask 'boilr' do
+class Boilr < Formula
+  desc "Boilerplate template manager that generates files or directories from template repositories"
+  homepage "https://github.com/solaegis/boilr"
+  url "https://github.com/solaegis/boilr/releases/download/v#{version}/boilr-#{version}-darwin_adm64.tgz"
   version "1.1.1"
   sha256 "788772739a22d3f4023758a0599f51b08fce1c84c4ae7cc6b199fdc260f8c9ef"
 
-  url "https://github.com/solaegis/boilr/releases/download/v#{version}/boilr-#{version}-darwin_adm64.tgz"
-  appcast 'https://github.com/solaegis/boilr/releases.atom'
-  name 'Boilr'
-  homepage 'https://github.com/solaegis/boilr'
-
-  binary 'boilr'
-
-  zap trash: ['~/.config/boilr']
+  def install
+    bin.install "boilr"
+  end
 end
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Homebrew tap for Boilr
-
+```bash
 tar cvzf boilr-<version>-darwin_adm64.tgz boilr
 add to the github release
 shasum -a 256 boilr-<version>-darwin_adm64.tgz
+```
 
 ```bash
 brew tap solaegis/boilr
