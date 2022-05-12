b06857292e230e12874d89264328e614e247ae8d
Lewis Burgess <Lewis.Burgess@acxiom.com>
2022-05-12 11:57:38 -0400

    hardcode-version

--- Formula/boilr.rb
+++ Formula/boilr.rb
@@ -1,7 +1,7 @@
 class Boilr < Formula
   desc "Boilerplate template manager that generates files or directories from template repositories"
   homepage "https://github.com/solaegis/boilr"
-  url "https://github.com/solaegis/boilr/releases/download/v#{version}/boilr-#{version}-darwin_adm64.tgz"
+  url "https://github.com/solaegis/boilr/releases/download/v1.1.1/boilr-1.1.1-darwin_adm64.tgz"
   version "1.1.1"
   sha256 "788772739a22d3f4023758a0599f51b08fce1c84c4ae7cc6b199fdc260f8c9ef"
 
