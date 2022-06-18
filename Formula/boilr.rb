af94e24d0c1bd6c44372203d511742438c47b9f0
Lewis Burgess <Lewis.Burgess@acxiom.com>
2022-06-17 21:27:53 -0400

    v1.1.2

--- Formula/boilr.rb
+++ Formula/boilr.rb
@@ -1,9 +1,9 @@
 class Boilr < Formula
   desc "Boilerplate template manager that generates files or directories from template repositories"
   homepage "https://github.com/solaegis/boilr"
-  url "https://github.com/solaegis/boilr/releases/download/v1.1.1/boilr-1.1.1-darwin_adm64.tgz"
-  version "1.1.1"
-  sha256 "788772739a22d3f4023758a0599f51b08fce1c84c4ae7cc6b199fdc260f8c9ef"
+  url "https://github.com/solaegis/boilr/releases/download/v1.1.2/boilr-1.1.2-darwin_amd64.tgz"
+  version "1.1.2"
+  sha256 "015ff971b57f4c525e7ed34571ee8aa2e79a1920b73fec5be8da530f10afa85d"
 
   def install
     bin.install "boilr"
